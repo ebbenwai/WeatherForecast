c458ce1d3e18e30cf967a39771fc98865ee3f444
Amir <amir080121@gmail.com>
2024-06-17 11:39:23 +0300

    Init commit

---
+++ app/src/main/java/com/example/weatherapp/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.example.weatherapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val CardBg = Color(0xFFBCF4FF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
